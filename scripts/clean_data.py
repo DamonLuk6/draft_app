3b0b400271abd03c5a02dc6616f964131679b11a
Damon Luk <damonluk924@gmail.com>
2026-07-28 21:22:46 -0400

    basic cleaning done

---
+++ scripts/clean_data.py
@@ -0,0 +1,32 @@
+#luk 
+
+import pandas as pd 
+
+raw_player_data = pd.read_csv("data/raw_player_registration.csv")
+
+raw_player_data_column_subset = raw_player_data[['first_name', \
+                                     'last_name', \
+                                     'email_address', \
+                                     'gender_id', \
+                                     'age', \
+                                     'shirt_size', \
+                                     'USAU_member_id', \
+                                     'baggage_group_id', \
+                                     'baggage', \
+                                     'Captain Interest', \
+                                     'Experience Count', \
+                                     'Level of Play', \
+                                     'Height', \
+                                     'Experience List', \
+                                     'Throwing', \
+                                     'Cutting', \
+                                     'Athleticism', \
+                                     'Endurance', \
+                                     'Role', \
+                                     'Names', \
+                                     'New Player', \
+                                     'Missing Games', \
+                                     'Status']]
+
+print(raw_player_data.head(10))
+print(raw_player_data_column_subset.head(10))
